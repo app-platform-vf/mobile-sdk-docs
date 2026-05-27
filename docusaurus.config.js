f02c913704b7c43292ae1ed4d8b04cabfc2d7064
chinbh1 <chinbh1@viettel.com.vn>
2026-05-27 15:31:14 +0700

    fix: use babel.config.cjs for CJS/ESM compat, remove missing favicon ref

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,6 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'VDO SDK Docs',
   tagline: 'Documentation for VDO Mobile SDKs',
-  favicon: 'img/favicon.ico',
 
   url: 'https://app-platform-vf.github.io',
   baseUrl: '/sdk-docs/',
